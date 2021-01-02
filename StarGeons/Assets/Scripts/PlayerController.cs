08d6595c1f6f3a31459b6bb7b33ef8dcd37cd1f1
stachurski2k4 <staskacp412@gmail.com>
2021-01-02 20:54:40 +0100

    Little update

---
+++ StarGeons/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Control{
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    CharacterController characterController;
+    //TO DO
+    //InputHandler inputHandler;
+     float maxForwardSpeed=4f;
+     float turnSpeed=10f;
+     float jumpSpeed=10f;
+     float crouchSpeedFactor=0.5f;
+    float aimSpeedFraction=0.7f;
+     float runSpeedFactor=1.5f;
+    float desiredForwardSpeed;
+    float verticalSpeed=0f;
+    float currentVelocity;
+    bool isGrounded=true;
+    bool isCrouching=false;
+    bool readyToJump=false;
+    bool isRunning=false;
+    Vector3 additiveForce;
+    float currentSpeed;
+    Quaternion targetRotation;
+    //These are the values i think will be needed
+    void Start()
+    {
+        //inputHandler=GetComponent<InputHandler>();
+        characterController=GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //To do : main Loop
+    }
+    //To do : Use it as collider
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+    }
+}
+}
