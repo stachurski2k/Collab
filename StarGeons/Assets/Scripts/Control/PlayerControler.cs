b1f7045905777b2e6a46992099ffc69a1a54684d
stachurski2k4 <staskacp412@gmail.com>
2021-01-11 14:06:17 +0100

    mazes

---
+++ StarGeons/Assets/Scripts/Control/PlayerControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PlayerState{IDLE,WALKING,CROUCHING,RUNNING,SLIDING,}
+[RequireComponent(typeof(InputHandler))]
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] LayerMask ground;
+    [SerializeField] Transform groundCheck;
+    [Header("Movement Variables")]
+    [SerializeField] float walkSpeed=5f;
+    [SerializeField] float sprintMultiplier=1.5f;
+    [SerializeField]float crouchMultiplier=.7f;
+    [SerializeField] float airControlMultiplier=.6f;
+    [SerializeField] float jumpForce=10f;
+    [SerializeField] float gravity=20f;
+    [Header("Camera Variables")]
+    [SerializeField] Vector2 smothing=new Vector2(3,3);
+    CharacterController controller;  
+    InputHandler input;
+    Transform cam;
+    bool isGrounded=true;
+    void Start()
+    {
+        input=GetComponent<InputHandler>();
+        cam=Camera.main.transform;
+    }
+
+    void Update()
+    {
+        UpdateCam();
+    }
+    void UpdateCam(){
+        
+    }
+}
---
+++ StarGeons/Assets/Scripts/Utils/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    static System.Random sysR=new System.Random();
+    public static void Shuffle<T>(this IList<T> list){
+        int n =list.Count;
+        while(n>1){
+            n--;
+            int k=sysR.Next(n+1);
+            T value=list[n];
+            list[n]=list[k];
+            list[k]=value;
+        }
+    }
+}
