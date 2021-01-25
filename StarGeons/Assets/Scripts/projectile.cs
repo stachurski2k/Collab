e3ae7f0bd049b3f8756b6732b432aef77eeb9d14
begadwael <begad.mestar@gmail.com>
2021-01-25 22:58:37 +0400

    basic gun and bullets

---
+++ StarGeons/Assets/Scripts/projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    [HideInInspector] public float lifeTime;
+    [HideInInspector] public float Damage;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print($"hit {other.transform.name}");
+        Destroy(gameObject);
+
+        // do things
+
+        // play VFX
+    }
+}
